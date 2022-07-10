646bbd62bc017ed6edfa09cdbe87281d83468a1a
agvan <agvandyke25@gmail.com>
2022-07-10 13:55:00 -0500

    updated schema.sql & seeds.sql to reflect updated table name & started departments queries in server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE if exists department;
+DROP TABLE if exists departments;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT PRIMARY KEY,
     name VARCHAR (30)
 );
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //imported modules
 const express = require("express");
 const inquirer = require("inquirer");
-const db = require("./db/connection");
-const apiRoutes = require("./routes/apiRoutes");
+//const db = require("./db/connection");
+//const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,18 +12,51 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Directs the app to use apiRoutes
-app.use("/api", apiRoutes);
+//app.use("/api", apiRoutes);
+
+//All departments
+db.query(`SELECT * FROM departments`, (err, rows) => {
+  console.log(rows);
+});
 
 // The default response for any other request.
 app.use((req, res) => {
   res.status(404).end();
 });
 
+//GET a single department
+db.query(`SELECT * FROM departments WHERE id =1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+//Delete a department
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+//Create a department
+const sql = `INSERT INTO departments (id, name)
+             VALUES (?, ?)`;
+const params = [5, "Human Resources"];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 //test route for the server
 //app.get("/", (req, res) => {
-  //res.json({
-    //message: "Hello World",
-  //});
+//res.json({
+//message: "Hello World",
+//});
 //});
 
 // Server is started after a database connection
