5e756e5c59da315b518dffa63150370dd9049c4e
agvan <agvandyke25@gmail.com>
2022-07-10 10:48:49 -0500

    started imported necessary modules

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const db = ('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
