6ddd284d920573aac403b019b9cb61a3e94366c0
agvan <agvandyke25@gmail.com>
2022-07-10 12:09:42 -0500

    added imported modules and module exports

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+//imported modules
+const express = require('express');
+const router = express.Router();
+const inquirer = require("inquirer");
+
+
+
+//module exports for router
+module.exports = router;
