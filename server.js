0591ff40d6ac5a5321a4a7e98e4470d8c7cbe718
agvan <agvandyke25@gmail.com>
2022-07-10 10:51:52 -0500

    added express middleware

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //imported modules
-const express = require('express');
-const db = ('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const express = require("express");
+const db = "./db/connection";
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
