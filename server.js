acc367a5e79b204b080b81d8bebb512126507960
agvan <agvandyke25@gmail.com>
2022-07-10 10:59:41 -0500

    added inquirer to imported modules

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //imported modules
-const express = require("express");
-const db = "./db/connection";
-const apiRoutes = require("./routes/apiRoutes");
+const express = require('express');
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
