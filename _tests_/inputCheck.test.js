2cdd8f2f514ac7111fe6d2822866634f4e9a01f9
agvan <agvandyke25@gmail.com>
2022-07-10 11:29:52 -0500

    added second test

--- _tests_/inputCheck.test.js
+++ _tests_/inputCheck.test.js
@@ -6,3 +6,13 @@ test("inputCheck() returns null when all properties exist", () => {
 
   expect(inputCheck(obj, "name")).toBe(null);
 });
+
+test('inputCheck() returns an object when a property is missing', () => {
+    const obj = {name: 'alice', occupation: ''};
+  
+    expect(inputCheck(obj, 'name', 'occupation')).toEqual(
+      expect.objectContaining({
+        error: expect.stringContaining('No occupation specified')
+      })
+    );
+  });
