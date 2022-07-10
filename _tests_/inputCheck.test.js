b5ecbdecdd1152e176d5cf8df7f0abc0b7482f09
agvan <agvandyke25@gmail.com>
2022-07-10 11:23:05 -0500

    wrote the test

--- _tests_/inputCheck.test.js
+++ _tests_/inputCheck.test.js
@@ -1,1 +1,8 @@
-const inputCheck = require('../utils/inputCheck');
+const { default: test } = require("node:test");
+const inputCheck = require("../utils/inputCheck");
+
+test("inputCheck() returns null when all properties exist", () => {
+  const obj = { name: "winston" };
+
+  expect(inputCheck(obj, "name")).toBe(null);
+});
