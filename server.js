727eb24a229a18a025f74c05519a5e0096ec602a
agvan <agvandyke25@gmail.com>
2022-07-10 11:09:24 -0500

    established connection between database and server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //imported modules
-const express = require('express');
-const inquirer = require('inquirer');
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const express = require("express");
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,9 +12,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Directs the app to use apiRoutes
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 // The default response for any other request.
 app.use((req, res) => {
-    res.status(404).end();
-})
+  res.status(404).end();
+});
+
+// Server is started after a database connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("You are connected to the database!");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
