1aee694dc70a30624c938e66bc5b3251b118d9ef
agvan <agvandyke25@gmail.com>
2022-07-10 11:17:17 -0500

    created createConnection for mysql

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,14 @@
 const mysql = require("mysql2");
+
+//Connect to the database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Winston123@$",
+        database: "employee manager",
+    },
+    console.log("Connected to the employee manager database.")
+);
+
+module.exports = db;
