170a16685d35c2392a86a493405895fd215dbd6e
agvan <agvandyke25@gmail.com>
2022-07-11 11:11:22 -0500

    changed how employee database is created & how roles and departments tables are created

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,15 @@
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE departments(
+    departments_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL NOT NULL,
-    department_id INTEGER, 
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+CREATE TABLE roles(
+    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
+
+
+
 
 
 
