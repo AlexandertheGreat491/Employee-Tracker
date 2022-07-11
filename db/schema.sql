2edc0838c7c540c575307c9d906ebf68fc2d9404
agvan <agvandyke25@gmail.com>
2022-07-11 09:24:56 -0500

    created table for roles

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE departments (
     id INT PRIMARY KEY,
     name VARCHAR (30)
 );
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
