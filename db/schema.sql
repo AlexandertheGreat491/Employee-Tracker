e9a72cca3352979185991b448db54e71b840b55e
agvan <agvandyke25@gmail.com>
2022-07-11 10:18:26 -0500

    attempting to create tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,17 @@
-DROP TABLE if exists departments;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
 
 CREATE TABLE departments (
     id INT PRIMARY KEY,
-    name VARCHAR (30)
+    name VARCHAR (30) 
 );
 
 CREATE TABLE roles (
     id INT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR (30),
     salary DECIMAL,
     department_id INT
+    CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE NULL
 );
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO departments (id, name)
 VALUES
 (1, 'Sales'),
@@ -5,13 +6,4 @@ VALUES
 (3, 'Finance'),
 (4, 'Legal');
 
-INSERT INTO roles (id, title, salary)
-VALUES
-(1, 'Sales Lead', 100000),
-(2, 'Salesperson', 80000),
-(3, 'Lead Engineer', 150000),
-(4, 'Software Engineer', 120000),
-(5, 'Account Manager', 160000),
-(6, 'Accountant', 125000),
-(7, 'Legal Team Lead', 250000),
-(8, 'Lawyer', 190000);
+
