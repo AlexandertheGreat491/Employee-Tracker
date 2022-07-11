db3d2ce7818a597ab98cfd1b2033e919dac4d575
agvan <agvandyke25@gmail.com>
2022-07-11 09:39:37 -0500

    added values into roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,13 @@ VALUES
 (3, 'Finance'),
 (4, 'Legal');
 
+INSERT INTO roles (id, title, salary)
+VALUES
+(1, 'Sales Lead', 100000),
+(2, 'Salesperson', 80000),
+(3, 'Lead Engineer', 150000),
+(4, 'Software Engineer', 120000),
+(5, 'Account Manager', 160000),
+(6, 'Accountant', 125000),
+(7, 'Legal Team Lead', 250000),
+(8, 'Lawyer', 190000);
