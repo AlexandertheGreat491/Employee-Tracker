6f13989dd57763db5dc0ab964f9a7abed063e305
agvan <agvandyke25@gmail.com>
2022-07-12 11:51:50 -0500

    added require statement for console.table

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //imported modules
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
+const consoleTable = require('console.table');
 const db = require("./db/connection");
 
 
