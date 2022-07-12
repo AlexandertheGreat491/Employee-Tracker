b3b9fcd0e140c0f42d39c4c3ae09d73b75fd556b
agvan <agvandyke25@gmail.com>
2022-07-12 11:57:24 -0500

    moved code over from connection.js to server.js, made quotation marks consistent, & deleted redundant code

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 //imported modules
 const mysql = require('mysql2');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const consoleTable = require('console.table');
-const db = require("./db/connection");
+const db = require('./db/connection');
+
+//Connect to the database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Winston123@$",
+        database: "employee",
+    },
+    console.log("Connected to the employee database.")
+);
+
+module.exports = db;
 
 
 
