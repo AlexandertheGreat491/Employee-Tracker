5400dbe1a6428c3e2bfe9940240753d07a1cdaa1
agvan <agvandyke25@gmail.com>
2022-07-12 12:09:35 -0500

    started writing prompts

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
-const db = require('./db/connection');
+const { appendFile } = require('fs');
+const { options, choices } = require('yargs');
+
+
 
 //Connect to the database
-const db = mysql.createConnection(
+const createConnection = mysql.createConnection(
     {
         host: "localhost",
+        port: 3001,
         user: "root",
         password: "Winston123@$",
         database: "employee",
@@ -15,7 +19,36 @@ const db = mysql.createConnection(
     console.log("Connected to the employee database.")
 );
 
-module.exports = db;
+//connects to server and database
+createConnection.connect(function(err){
+  if (err) throw err;
+  choices();
+})
+
+// User is prompted with a list of options to choose from.
+function choices() {
+  inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'Welcome to the employee database! Please select what you would like to do.',
+    choices: [
+      'View all employees',
+      'View all departments',
+      'View all roles',
+      'Add an employee',
+      'Add a role',
+      'Update employee role',
+      'Delete an employee',
+      'EXIT'
+    ]
+  })
+}
+
+
+
+
+
+
 
 
 
